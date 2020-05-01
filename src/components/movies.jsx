30c0b05cf8d458f8a736c552067a9b6e061ccef5
Jeon <jbsgo87@gmail.com>
2020-05-01 22:34:17 +0900

    added the delete button

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,26 +3,38 @@ import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = { movies: getMovies() };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: movies }); //this.setState({ movies });
+  };
   render() {
     return (
-      <table className="table table-bordered">
+      <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
             <th scope="col">Title</th>
             <th scope="col">Genre</th>
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie, index) => (
-            <tr>
-              <th>{index}</th>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>

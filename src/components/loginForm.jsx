cfc197a4347f99a18eecd5398234bad2942490f2
Jeon <jbsgo87@gmail.com>
2020-05-03 08:31:20 +0900

    added the code for refs for input ui

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef(); // ref를 많이 사용하면 좋지않음.
+
+  componentDidMount() {
+    this.username.current.focus();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const username = this.username.current.value;
+    // 왠만하면 아래처럼 사용하면 좋지 않음. dom을 사용하는것이 유지/테스트에 유리.
+    //  const username = document.getElementById("username").value;
+    console.log("submit", username);
+  };
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">UserName</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>

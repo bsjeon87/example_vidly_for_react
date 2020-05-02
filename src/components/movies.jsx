dc6d4e6d9af9680eff61daf8490593272527cbee
Jeon <jbsgo87@gmail.com>
2020-05-02 18:07:36 +0900

    refactoring : moving Responsibility for sorting

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -26,14 +26,7 @@ class Movies extends Component {
     this.setState({ movies: movies }); //this.setState({ movies });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -85,6 +78,7 @@ class Movies extends Component {
           <p> Showing {count} movies in the database.</p>
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}

f8d05d401af8394e0efba48e07c434be1a7b3875
Jeon <jbsgo87@gmail.com>
2020-05-04 05:13:02 +0900

    install Joi & replace validation code with Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -7,6 +8,11 @@ class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
     errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("password"),
   };
 
   componentDidMount() {
@@ -34,14 +40,14 @@ class LoginForm extends Component {
   };
 
   validate = () => {
-    const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
 
-    return Object.keys(errors).length === 0 ? null : errors;
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+
+    return errors;
   };
 
   handleSubmit = (e) => {
